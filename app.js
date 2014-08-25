aa1548311af73bb963474eba465d6d16a87e7367
Tristan Davies <github@tristan.io>
2014-08-25 16:42:58 -0400

    make example page more usable

--- app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('app', ['liveaddress']);
-
-app.controller('testCtrl', ['$scope', function($scope){
-  $scope.address = '';
-
-  $scope.update = function(data){
-    console.log(data);
-  };
-}]);
